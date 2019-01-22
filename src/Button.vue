3570ddf9361b01d39b3b48a804225b35dae1e4dc
v_lzhicliu <v_lzhicliu@tencent.com>
2019-01-22 20:49:13 +0800

    test

---
+++ src/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="g-button">点我</button>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+  .g-button{
+      padding: 5px;
+  }
+</style>
+
+
