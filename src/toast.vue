f0706f2cbe3d51b2baebd16e02cb18860dc8556a
v_lzhicliu <v_lzhicliu@tencent.com>
2019-01-30 17:19:47 +0800

    优化弹出动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="toast" ref="toast" :class="toastClass">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
+  <div class="wrapper" :class="toastClass">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
+      <div class="line" ref="line"></div>
+      <div class="close" v-if="buttonClose" @click="onButtonClose">{{buttonClose.text}}</div>
     </div>
-    <div class="line" ref="line"></div>
-    <div class="close" v-if="buttonClose" @click="onButtonClose">{{buttonClose.text}}</div>
   </div>
 </template>
 <script>
@@ -35,30 +37,36 @@ export default {
       default: false
     },
     position: {
-        type: String,
-        default: 'top',
-        validator(value){
-            return ['top','bottom','middle'].indexOf(value) > -1
-        }
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "middle"].indexOf(value) > -1;
+      }
+    },
+    OnlyOneToast: {
+      type: Boolean,
+      default: true
     }
   },
   mounted() {
     this.handerAutoClose();
-    this.initStyles()
+    this.initStyles();
   },
   computed: {
-      toastClass(){
-        return {
-            [`position-${this.position}`]: true
-        }
-      }
+    toastClass() {
+      return {
+        [`position-${this.position}`]: true
+      };
+    }
   },
   methods: {
-    initStyles(){
-        this.$nextTick(() => {
-          this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
-          // this.$refs.toast.style.height >>> undefined 无法获取外联样式
-        },1000)
+    initStyles() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height = `${
+          this.$refs.toast.getBoundingClientRect().height
+        }px`;
+        // this.$refs.toast.style.height >>> undefined 无法获取外联样式
+      }, 1000);
     },
     handerAutoClose() {
       if (this.autoClose) {
@@ -70,6 +78,7 @@ export default {
     close() {
       // dom元素remove
       this.$el.remove();
+      this.$emit("close");
       // 组件死掉，注销绑定的事件
       this.$destroy();
     },
@@ -90,14 +99,66 @@ export default {
 $font-size: 14px;
 $height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
-.toast {
-  font-size: $font-size;
+.wrapper{
   position: fixed;
-  top: 0;
   left: 50%;
+  transform: translateX(-50%);
+   &.position-top {
+    top: 0;
+    .toast {
+      animation: fade-down-in 0.8s;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+  }
+  &.position-middle {
+    top: 50%;
+    animation: fade-in 0.8s;
+    transform: translate(-50%,-50%);
+  }
+  &.position-bottom {
+    top: inherit;
+    bottom: 0;
+    .toast {
+      animation: fade-up-in 0.8s;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+  }
+}
+.toast {
+  @keyframes fade-up-in {
+    0% {
+      opacity: 0;
+      transform: translateY(100%)
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0%)
+    }
+  }
+  @keyframes fade-down-in {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%)
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0%)
+    }
+  }
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  font-size: $font-size;
+  top: 0;
   line-height: 1.7;
   min-height: $height;
-  transform: translateX(-50%);
   display: flex;
   align-items: center;
   color: white;
@@ -105,19 +166,9 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   padding: 0 16px;
   border-radius: 4px;
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
-  &.position-top{
-     top: 0
-  }
-  &.position-middle{
-     top: 50%;
-     transform: translate(-50%,-50%);
-  }
-  &.position-bottom{
-     top: inherit;
-     bottom: 0;
-  }
-  .message{
-      padding: 8px 0;
+ 
+  .message {
+    padding: 8px 0;
   }
   .line {
     height: 100%;
