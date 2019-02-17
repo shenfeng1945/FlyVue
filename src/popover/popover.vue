23dc50710b79e8b3e853c0363f621a08b11f9adb
shenfeng1945 <823902826@qq.com>
2019-02-17 22:59:14 +0800

    添加popover可点击功能

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,48 +1,112 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-       <div class="content-wrapper" v-if="visiable" @click.stop>
-           <slot name="content"></slot>
-       </div>
-       <slot></slot>
+  <div class="popover"
+       ref="popover">
+    <div class="content-wrapper"
+         ref="contentWrapper"
+         v-if="visiable">
+      <slot name="content"></slot>
     </div>
+    <span ref="triggerWrapper" style="display:inline-block;">
+      <slot></slot>
+    </span>
+  </div>
 </template>
 <script>
 export default {
-    name: 'FlyPopover',
-    data(){
-        return {
-            visiable: false
-        }
+  name: "GuluPopover",
+  props: {
+      trigger: {
+          type: String,
+          default: 'click',
+          validator(val){
+              return ['click','mouse'].indexOf(val) > -1;
+          }
+      }
+  },
+  data() {
+    return {
+      visiable: false
+    };
+  },
+  mounted(){
+     this.addTriggerEvent()
+  },
+  beforeDestroy(){
+     this.removeContentWrapper()
+     this.removeTriggerEvent()
+  },
+  methods: {
+    putBackContent(){
+      const { contentWrapper,popover } = this.$refs;
+      popover.appendChild(contentWrapper);
     },
-    methods:{
-        xxx(){
-            this.visiable = !this.visiable
-            if(this.visiable === true){
-                this.$nextTick(() => {
-                    const yyy = () => {
-                        this.visiable = false
-                        console.log('object')
-                        document.removeEventListener('click',yyy)
-                    }
-                    document.addEventListener('click',yyy)
-                })
-            }
-        }
+    onClickDocument(e){
+      const { contentWrapper, triggerWrapper} = this.$refs
+       if(triggerWrapper.contains(e.target))return;
+       if(contentWrapper.contains(e.target))return;
+       this.close()
+    },
+    computePosition(){
+      const { contentWrapper, triggerWrapper} = this.$refs
+      if(!contentWrapper)return;
+      document.body.appendChild(contentWrapper)
+      const {width,height,top,left} = triggerWrapper.getBoundingClientRect()
+      const {height: height2} = contentWrapper.getBoundingClientRect();
+      contentWrapper.style.left = left + window.scrollX + 'px';
+      contentWrapper.style.top = top - height2 + window.scrollY + 'px';
+    },
+    addTriggerEvent(){
+       const {triggerWrapper} = this.$refs;
+       if(this.trigger === 'click'){
+         triggerWrapper.addEventListener('click', this.onClick)
+       }else{
+         triggerWrapper.addEventListener('mouseenter', this.open)
+         triggerWrapper.addEventListener('mouseover', this.close)
+       }
+    },
+    removeTriggerEvent(){
+        const {triggerWrapper} = this.$refs;
+       if(this.trigger === 'click'){
+         triggerWrapper.removeEventListener('click', this.onClick)
+       }else{
+         triggerWrapper.removeEventListener('mouseenter', this.open)
+         triggerWrapper.removeEventListener('mouseover', this.close)
+       }
+    },
+    onClick(e){
+       const {triggerWrapper} = this.$refs;
+       if(triggerWrapper.contains(e.target)){
+          if(this.visiable){
+              this.close()
+          }else{
+              this.open()
+          }
+       }
+    },
+    close(){
+        this.visiable = false;
+        document.removeEventListener('click',this.onClickDocument)
+    },
+    open(){
+        this.visiable = true;
+        this.$nextTick(() => {
+          this.computePosition()
+          document.addEventListener('click', this.onClickDocument)
+        })
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
-  .popover{
-    position: relative;
-    margin: 100px;
-      .content-wrapper{
-          position: absolute;
-          border: 1px solid red;
-          bottom: 100%;
-          left: 0;
-          box-shadow: 0 0 3px rgba(0,0,0,.5);
-      }
+.popover {
+  margin: 100px;
+  position: relative;
+  
+}
+.content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   }
 </style>
-
 
