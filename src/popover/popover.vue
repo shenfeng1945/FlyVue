bed1896f618df0bb51e2d5dbf615cbb1e527cb1a
v_lzhicliu <v_lzhicliu@tencent.com>
2019-02-26 13:20:38 +0800

    使用表驱动重构代码-四方click

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="popover" ref="popover">
-    <div class="content-wrapper" ref="contentWrapper" v-if="visiable">
+    <div
+      class="content-wrapper"
+      :class="[`position-${this.position}`]"
+      ref="contentWrapper"
+      v-if="visiable"
+    >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display:inline-block;">
@@ -64,8 +69,26 @@ export default {
         height: height2,
         width: width2
       } = contentWrapper.getBoundingClientRect();
-      contentWrapper.style.left = left + window.scrollX + "px";
-      contentWrapper.style.top = top + window.scrollY + "px";
+      let positions = {
+        top: {
+          top: top + window.scrollY,
+          left: left + window.scrollX
+        },
+        bottom: {
+          top: top + height + window.scrollY,
+          left: left + window.scrollX
+        },
+        left: {
+          top: top + (height - height2)/2 +  window.scrollY,
+          left: left - width2 + window.scrollX
+        },
+        right: {
+          top: top + (height - height2)/2 +  window.scrollY,
+          left: left + width + window.scrollX
+        }
+      }
+      contentWrapper.style.left = positions[this.position].left + "px";
+      contentWrapper.style.top = positions[this.position].top + "px";
     },
     addTriggerEvent() {
       const { triggerWrapper } = this.$refs;
@@ -113,36 +136,79 @@ export default {
 $border-color: #333;
 $border-radius: 4px;
 .popover {
-  margin: 100px;
   position: relative;
 }
 .content-wrapper {
   position: absolute;
   border: 1px solid $border-color;
   // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  filter: drop-shadow(0 0 3px rgba(0,0,0,.5));
+  filter: drop-shadow(0 0 3px rgba(0, 0, 0, 0.5));
   max-width: 20em;
-  background:white;
+  background: white;
   word-break: break-all;
   border-radius: $border-radius;
-  padding: .5em 1em;
-  transform: translateY(-100%);
-  margin-top: -10px;
-  &::before,&::after{
-    content: '';
+  padding: 0.5em 1em;
+  &::before,
+  &::after {
+    content: "";
     position: absolute;
     display: block;
-    width:0;height:0;
+    width: 0;
+    height: 0;
     border: 8px solid transparent;
-    left: 10px;
   }
-  &::before{
-    border-top-color: $border-color;
-    top:100%;
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before {
+      border-top-color: $border-color;
+      top: 100%;
+      left: 10px;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+      left: 10px;
+    }
   }
-  &::after{
-    border-top-color: white;
-    top: calc(100% - 1px);
+  &.position-bottom {
+    margin-top: 10px;
+    &::before {
+      border-bottom-color: $border-color;
+      bottom: 100%;
+      left: 10px;
+    }
+    &::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+      left: 10px;
+    }
+  }
+   &.position-left {
+    margin-left: -10px;
+    &::before {
+      border-left-color: $border-color;
+      left: 100%;
+      top: 10px;
+    }
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+      top: 10px;
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before {
+      border-right-color: $border-color;
+      right: 100%;
+      top: 10px;
+    }
+    &::after {
+      border-right-color: white;
+      right: calc(100% - 1px);
+      top: 10px;
+    }
   }
 }
 </style>
