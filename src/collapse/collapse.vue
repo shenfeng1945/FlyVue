7b2646e1ad840135e2e14532c8f16f12de3b4ce1
shenfeng1945 <823902826@qq.com>
2019-03-03 00:43:49 +0800

    父组件单向控制数据，子组件通过数据来展示

--- src/collapse/collapse.vue
+++ src/collapse/collapse.vue
@@ -4,26 +4,47 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
   name: "FlyCollapse",
   props: {
-      single: {
-          type: Boolean,
-          default: false
-      }
+    single: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Array
+    }
   },
-  data(){
-      return {
-          eventBus: new Vue()
-      }
+  data() {
+    return {
+      eventBus: new Vue()
+    };
   },
-  provide(){
-     if(this.single){
-      return {
-          eventBus: this.eventBus
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  mounted() {
+    this.eventBus.$emit("update:selected", this.selected);
+    // 监听子元素，将name数组传给本身
+    this.eventBus.$on("update:addSelected", name => {
+      let selectedArray = Object.assign([],this.selected)
+      if (this.single) {
+        selectedArray = [];
       }
-     }
+      selectedArray.push(name);
+      this.$emit("update:selected", selectedArray);
+      this.eventBus.$emit("update:selected", selectedArray);
+    });
+    this.eventBus.$on("update:removeSelected", name => {
+      let selectedArray = Object.assign([],this.selected)
+      let index = selectedArray.indexOf(name);
+      selectedArray.splice(index, 1);
+      this.$emit("update:selected", selectedArray);
+      this.eventBus.$emit("update:selected", selectedArray);
+    });
   }
 };
 </script>
--- src/collapse/collapseItem.vue
+++ src/collapse/collapseItem.vue
@@ -19,6 +19,10 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
   },
   data() {
@@ -28,8 +32,10 @@ export default {
   },
   mounted() {
     this.eventBus &&
-      this.eventBus.$on("update", vm => {
-        if (vm !== this) {
+      this.eventBus.$on("update:selected", selectedArray => {
+        if (selectedArray.indexOf(this.name) > -1) {
+          this.isOpen = true;
+        } else {
           this.isOpen = false;
         }
       });
@@ -37,10 +43,9 @@ export default {
   methods: {
     triggleClick() {
       if (this.isOpen) {
-        this.isOpen = false;
+        this.eventBus.$emit("update:removeSelected", this.name);
       } else {
-        this.isOpen = true;
-        this.eventBus && this.eventBus.$emit("update", this);
+        this.eventBus.$emit("update:addSelected", this.name);
       }
     }
   },
@@ -56,12 +61,26 @@ $border-radius: 4px;
     margin-top: -1px;
     margin-left: -1px;
     margin-right: -1px;
+    min-height: 32px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
   }
   &:first-child {
     > .title {
       border-top-left-radius: $border-radius;
       border-top-right-radius: $border-radius;
     }
+  }
+  &:last-child {
+    > .title:last-child {
+      border-bottom-left-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
+      border-bottom: none;
+    }
+  }
+  .content {
+    padding: 8px;
   }
 }
 </style>
