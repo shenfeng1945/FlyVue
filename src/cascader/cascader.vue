60072079b081c717799c9607bc23224e1df757a2
shenfeng1945 <823902826@qq.com>
2019-03-03 20:09:39 +0800

    添加级联内容递归展示

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cascader">
+    <div class="trriger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <f-cascasder-item v-for="(sourcesItem,index) in sources" :key="index" :sourcesItem="sourcesItem"></f-cascasder-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import FlyCascaderItem from "./cascaderItem";
+export default {
+  name: "FlyCascader",
+  props: {
+    sources: Array
+  },
+  components: {
+    'f-cascasder-item': FlyCascaderItem
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.cascader {
+  padding: 100px;
+}
+</style>
---
+++ src/cascader/cascaderItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cascader-item">
+    <div class="name">{{sourcesItem.name}}</div>
+    <template v-if="sourcesItem.children">
+      <FlyCascaderItem v-for="(item,i) in sourcesItem.children" :key="i" :sourcesItem="item"></FlyCascaderItem>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlyCascaderItem",
+  props: {
+      sourcesItem: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.cascader-item {
+}
+</style>
