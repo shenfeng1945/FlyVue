bfc4f7d1e3b4f75bd8ab6b144c38b5a89897d297
shenfeng1945 <823902826@qq.com>
2019-03-17 22:51:59 +0800

    更新单元测试，使用vue cli 3自带的

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,75 @@
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import Button from '@/button/button.vue'
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+chai.use(sinonChai)
+
+describe('Button', () => {
+    it('存在', () => {
+        expect(Button).to.be.ok
+    })
+    it('可以设置icon.', () => {
+        const wrapper = mount(Button,{
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        expect(wrapper.find('use').attributes()['href']).to.equal('#i-settings')
+    })
+    it('可以设置loading.', () => {
+        const wrapper = mount(Button,{
+            propsData: {
+                //  icon: 'settings',
+                loading: true
+            }
+        })
+        const vm = wrapper.vm
+        const useElements = vm.$el.querySelectorAll('use')
+        expect(useElements.length).to.equal(1)
+        expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    })
+    it('icon 默认的 order 是 1', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        }).$mount(div)
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+    it('设置 iconPosition 可以改变 order', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        }).$mount(div)
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+    it('点击 button 触发 click 事件', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'settings',
+            }
+        })
+        const vm = wrapper.vm;
+        // sinon提供的一个假函数，它知道自己被调用了
+        const callback = sinon.fake();
+        vm.$on('click', callback)
+        vm.$el.click()
+        expect(callback).to.have.been.called
+    })
+})
