810c414144a9baf98af15a87c42ec6020b5e043e
shenfeng1945 <823902826@qq.com>
2019-03-27 07:35:54 +0800

    使用jest完善测试用例

--- tests/unit/row.spec.js
+++ tests/unit/row.spec.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
-import Row from '../src/row'
-import Col from '../src/col'
+import Row from '@/row'
+import Col from '@/col'
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
-const expect = chai.expect;
 
 describe('Row', () => {
-    const Constructor = Vue.extend(Row)
-    let vm
     it('存在.', () => {
         expect(Row).to.be.ok
     })
-    it('接受 gutter 属性', (done) => {
+    xit('接受 gutter 属性', (done) => {
         // 默认代码是同步的，只要加了done，才表示可为异步。异步代码结束后，调用done表示异步执行完了
         Vue.component('f-row',Row)
         Vue.component('f-col',Col)
@@ -24,7 +23,7 @@ describe('Row', () => {
             <f-col span="12"></f-col>
           </f-row>
         `
-        vm = new Vue({
+        let vm = new Vue({
           el: div
         })
         Promise.resolve().then(() => {
@@ -39,17 +38,14 @@ describe('Row', () => {
             vm.$destroy()
         })
     })
-    it('接受 align 属性', () => {
-      const div = document.createElement('div')
-      document.body.appendChild(div)
-      vm = new Constructor({
+    xit('接受 align 属性', () => {
+      const wrapper = mount(Row,{
           propsData: {
               align: 'right'
           }
-      }).$mount(div)
+      })
+      const vm = wrapper.vm;
       const rowElement = vm.$el
       expect(getComputedStyle(rowElement).justifyContent).to.equal('flex-end')
-      div.remove()
-      vm.$destroy()
     })
 })
---
+++ tests/unit/toast.spec.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import Toast from '@/toast'
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Toast', () => {
+  it('存在', () => {
+      expect(Toast).to.be.ok
+  })
+  it('接受 autoClose/autoCloseDelay 属性', (done) => {
+      const wrapper = mount(Toast, {
+          propsData: {
+            autoClose: 1,
+          }
+      })
+      const vm = wrapper.vm
+      setTimeout(() => {
+        expect(document.body.contains(vm.$el)).to.eq(false)
+        done()
+      },1500)
+  })
+  it('接受 buttonClose 属性', () => {
+    const wrapper = mount(Toast, {
+        propsData: {
+            buttonClose: {
+                text: '关闭',
+                callback
+            }
+        }
+    })
+    const vm = wrapper.vm
+    const callback = sinon.fake()
+    wrapper.find('.close').trigger('click')
+    const closeButton = vm.$el.querySelector('.close')
+    closeButton.click()
+    expect(closeButton.textContent).to.eq('关闭')
+    expect(callback).to.have.been.called
+  })
+//   it('接受 enableHtml 属性', () => {
+//     const Constructor = Vue.extend(Toast)
+//     const vm = new Constructor({
+//         propsData: {
+//           enableHtml: true
+//         }
+//     })
+//     vm.$slots.default = ['<p id="test">hello</p>']
+//     vm.$mount()
+//     expect(vm.$el.querySelector('#test')).to.be.ok
+//     vm.$destroy()
+//   })
+//   it('接受 position 属性', () => {
+//     const div = document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor = Vue.extend(Toast)
+//     const vm = new Constructor({
+//         propsData: {
+//           position: 'bottom',
+//         }
+//     }).$mount(div)
+//     expect(vm.$el.classList.contains('position-bottom')).to.eq(true)
+//     vm.$el.remove()
+//     vm.$destroy()
+//   })
+})
