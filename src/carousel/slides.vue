b017e5962af2a3a9db9f3124c2dde14abcc56596
v_lzhicliu <v_lzhicliu@tencent.com>
2019-04-12 14:24:29 +0800

    支持移动端设备，前进和后退

--- src/carousel/slides.vue
+++ src/carousel/slides.vue
@@ -3,6 +3,9 @@
     class="f-slides"
     @mouseenter="onMouseEnter"
     @mouseleave="onMouseLeave"
+    @touchstart="onTouchStart"
+    @touchmove="onTouchMove"
+    @touchend="onTouchEnd"
   >
     <div class="f-slides-window">
       <div class="f-slides-wrapper">
@@ -18,12 +21,20 @@
           {{index}}
         </span>
       </div>
+      <div class="f-slides-control-left" @click="goPrev">
+        <f-icon name="left"></f-icon>    
+      </div>
+      <div class="f-slides-control-right" @click="goNext">
+        <f-icon name="right"></f-icon>    
+      </div>
     </div>
 
   </div>
 </template>
 
 <script>
+// 待完善部分: 样式，前进后退到头不能再点
+import Icon from '../Icon'
 export default {
   name: "FlySlides",
   props: {
@@ -37,11 +48,22 @@ export default {
       default: false
     }
   },
+  components:{
+    'f-icon': Icon
+  },
   data() {
     return {
       childrenLength: 0,
       lastSelectedIndex: -1,
-      timerId: null
+      timerId: null,
+      touchPosition: {
+        left: undefined,
+        top: undefined,
+        isTouch: false,
+      },
+      // 移动端最小检测距离
+      minDetectX: 30,
+      minDetectY: 20,
     };
   },
   computed: {
@@ -49,13 +71,13 @@ export default {
       return this.names.indexOf(this.selected);
     },
     names() {
-      return this.$children.map(vm => vm.name);
+      return this.$children.filter(vm => vm.$options.name === 'FlySlidesItem').map(vm => vm.name);
     }
   },
   mounted() {
     this.updateChildren();
     this.autoPlay && this.playAutomatically();
-    this.childrenLength = this.$children.length;
+    this.childrenLength = this.names.length;
   },
   updated() {
     this.updateChildren();
@@ -66,7 +88,8 @@ export default {
       this.$children.forEach(vm => {
         let reverse =
           this.selectedIndex > this.lastSelectedIndex ? false : true;
-        if (this.timerId) {
+        // 
+        if (this.timerId || this.touchPosition.isTouch) {
           if (
             this.lastSelectedIndex === this.names.length - 1 &&
             this.selectedIndex === 0
@@ -99,6 +122,45 @@ export default {
       this.pause();
     },
     onMouseLeave() {
+      this.autoPlay && this.playAutomatically();
+    },
+    onTouchStart(e){
+      // 只支持单点触控
+      if(e.touches.length > 1){return}
+      const {clientX:left ,clientY: top} = e.touches[0]
+      this.touchPosition = {left,top,isTouch: false}
+      this.pause();
+    },
+    onTouchMove(e){
+       let {clientX: curX, clientY: curY} = e.touches[0];
+       let { left, isTouch, top } = this.touchPosition;
+       if(isTouch){return};
+       if(curX > left && Math.abs(curX-left) > this.minDetectX && Math.abs(curY-top) < this.minDetectY){
+         // 左滑滑
+         this.goPrev();
+         this.touchPosition.isTouch = true
+       }
+       if(curX < left && Math.abs(curX-left) > this.minDetectX && Math.abs(curY-top) < this.minDetectY){
+        // 右滑
+        this.goNext();
+        this.touchPosition.isTouch = true
+       }
+    },
+    goNext(){
+      let nextIndex = this.selectedIndex + 1;
+      if(nextIndex === this.names.length){
+        nextIndex = 0
+      }
+      this.select(nextIndex)
+    },
+    goPrev(){
+      let nextIndex = this.selectedIndex - 1;
+      if(nextIndex === -1){
+        nextIndex = this.names.length - 1;
+      }
+      this.select(nextIndex)
+    },
+    onTouchEnd(){
       this.autoPlay && this.playAutomatically();
     },
     pause() {
@@ -154,6 +216,21 @@ export default {
         background: black;
         color: white;
       }
+    }
+    .f-slides-control-left,.f-slides-control-right{
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 1em;
+      height: 1em;
+      border: 1px solid;
+    }
+    .f-slides-control-left{
+      left: 0;
+      
+    }
+    .f-slides-control-right{
+      right: 0;
     }
   }
 }
