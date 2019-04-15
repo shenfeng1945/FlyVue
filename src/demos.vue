392869576e269162cb0fe98664e6392511ce5923
v_lzhicliu <v_lzhicliu@tencent.com>
2019-04-15 19:53:17 +0800

    支持多层级nav

--- src/demos.vue
+++ src/demos.vue
@@ -1,33 +1,38 @@
 <template>
   <div id="app">
     <f-nav :selected.sync="selected" :multiple="false">
-        <f-nav-item name="home">首页</f-nav-item>
-        <f-sub-nav name="about">
-          <template slot="title">关于</template>
-          <f-nav-item name="culture">企业文化</f-nav-item>
-          <f-nav-item name="company">公司规模</f-nav-item>
-          <f-nav-item name="contacts">联系电话</f-nav-item>
+      <f-nav-item name="home">首页</f-nav-item>
+      <f-sub-nav name="about">
+        <template slot="title">关于</template>
+        <f-nav-item name="culture">企业文化</f-nav-item>
+        <f-nav-item name="company">公司规模</f-nav-item>
+        <f-sub-nav>
+          <template slot="title">联系电话</template>
+          <f-nav-item name="wechat">微信</f-nav-item>
+          <f-nav-item name="qq">QQ</f-nav-item>
+          <f-nav-item name="phone">电话</f-nav-item>
         </f-sub-nav>
-        <f-nav-item name="hire">招聘</f-nav-item>
+      </f-sub-nav>
+      <f-nav-item name="hire">招聘</f-nav-item>
     </f-nav>
   </div>
 </template>
 <script>
-import Nav from './nav/nav'
-import NavItem from './nav/nav-item'
-import SubNav from './nav/sub-nav'
+import Nav from "./nav/nav";
+import NavItem from "./nav/nav-item";
+import SubNav from "./nav/sub-nav";
 export default {
   name: "Demos",
   components: {
-      'f-nav': Nav,
-      'f-nav-item': NavItem,
-      'f-sub-nav': SubNav
+    "f-nav": Nav,
+    "f-nav-item": NavItem,
+    "f-sub-nav": SubNav
   },
-  data(){
+  data() {
     return {
-      selected: ['home']
-    }
-  },
+      selected: ["home"]
+    };
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -36,7 +41,6 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-
 </style>
 
 
--- src/nav/sub-nav.vue
+++ src/nav/sub-nav.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="f-sub-nav">
-    <span>
+    <span @click="open = !open">
       <slot name="title"></slot>
     </span>
-    <div class="f-sub-nav-popover">
+    <div class="f-sub-nav-popover" v-show="open">
       <slot></slot>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "FlySubNav"
+  name: "FlySubNav",
+  data(){
+    return {
+      open: false
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
   .f-sub-nav{
       position: relative;
-      padding: 1em;
+      > span{
+        padding: 1em;
+        display: block;
+      }
       &-popover{
           position:absolute;
           top:100%;
           left: 0;
           border: 1px solid;
           white-space: nowrap;
+      }
+      .f-sub-nav .f-sub-nav-popover{
+        position: absolute;
+        left: 100%;
+        top: 0;
       }
   }
 </style>
