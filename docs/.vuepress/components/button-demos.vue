c3c73533f6bb25e2181e50e557959d92d0cea39d
shenfeng1945 <823902826@qq.com>
2019-05-01 09:21:50 +0800

    优化button demos

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <f-button>默认按钮</f-button>
-  </div>
+    <div>
+        <f-button>默认按钮</f-button>
+        <f-button type="primary">sdf</f-button>
+    </div>
 </template>
 <script>
-import Button from "../../../src/button/button";
-export default {
-  components: {
-    "f-button": Button
-  }
-};
+    import Button from "../../../src/button/button";
+
+    export default {
+        components: {
+            "f-button": Button
+        }
+    };
 </script>
 
