b4f43427be4ae1cff2f50a1470f73d7adf2404e1
v_lzhicliu <v_lzhicliu@tencent.com>
2019-05-20 20:22:29 +0800

    优化sticky功能

--- src/demos.vue
+++ src/demos.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
     <div class="scroll">
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
-      <p>6</p>
-      <p>7</p>
-      <p>8</p>
-      <p>9</p>
-      <p>10</p>
       <f-sticky>
         <div style="border: 1px solid red;">
           这是粘贴的内容
@@ -24,38 +14,9 @@
       <p>16</p>
       <p>17</p>
       <p>18</p>
-      <p>19</p>
-      <p>20</p>
-      <p>21</p>
-      <p>22</p>
-      <p>23</p>
-      <p>24</p>
-      <p>25</p>
-      <p>26</p>
-      <p>27</p>
-      <p>28</p>
-      <p>29</p>
-      <p>30</p>
-      <p>31</p>
-      <p>32</p>
-      <p>33</p>
-      <p>34</p>
-      <p>35</p>
-      <p>36</p>
-      <p>37</p>
-      <p>38</p>
-      <p>39</p>
-      <p>40</p>
-      <p>41</p>
-      <p>42</p>
-      <p>43</p>
-      <p>44</p>
-      <p>45</p>
-      <p>46</p>
-      <p>47</p>
-      <p>48</p>
-      <p>49</p>
-      <p>50</p>
+      <p>16</p>
+      <p>17</p>
+      <p>18</p>
     </div>
   </div>
 </template>
@@ -79,7 +40,7 @@ export default {
   box-sizing: border-box;
 }
   .scroll{
-    height: 300px;
+    // height: 300px;
     overflow: auto;
   }
 </style>
--- src/sticky/sticky.vue
+++ src/sticky/sticky.vue
@@ -1,22 +1,66 @@
 <template>
-  <div class="f-sticky" ref="wrapper">
-    <slot></slot>
+  <div class="f-sticky-wrapper" ref="wrapper" :style="{height}">
+    <div class="f-sticky" :class="classes" :style="{left,width}">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "FlySticky",
+  data() {
+    return {
+      sticky: false,
+      width: undefined,
+      left: undefined,
+      height: undefined
+    };
+  },
   mounted() {
-    this.$refs.wrapper.parentNode.addEventListener("scroll", e => {
-      console.log(e);
-      console.log("hi");
-    });
+    const { top } = this.topAndHeight();
+    this.windowScrollHandler = this._windowScrollHandler.bind(this);
+    window.addEventListener("scroll", this.windowScrollHandler);
+  },
+  beforeDestory() {
+    window.removeEventListener("scroll", this.windowScrollHandler);
+  },
+  methods: {
+    topAndHeight() {
+      const { top, height } = this.$refs.wrapper.getBoundingClientRect();
+      return { top: top + window.scrollY, height };
+    },
+    _windowScrollHandler() {
+      if (window.scrollY > top) {
+        const {
+          height,
+          left,
+          width
+        } = this.$refs.wrapper.getBoundingClientRect();
+        this.height = height + "px";
+        this.left = left + "px";
+        this.width = width + "px";
+        this.sticky = true;
+      } else {
+        this.sticky = false;
+      }
+    }
+  },
+  computed: {
+    classes() {
+      return {
+        sticky: this.sticky
+      };
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
 .f-sticky {
+  &.sticky {
+    position: fixed;
+    top: 0;
+  }
 }
 </style>
