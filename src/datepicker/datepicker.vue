e43d6d5a70ae878d1b659094861cb562f6478ecc
shenfeng1945 <823902826@qq.com>
2019-05-21 07:21:04 +0800

    input 接受props作为placeholder

--- src/datepicker/datepicker.vue
+++ src/datepicker/datepicker.vue
@@ -1,25 +1,75 @@
 <template>
   <div class="f-date-picker">
-    <label>
-      <f-input type="text" :placeholder="placeholder" />
-    </label>
+    <f-input type="text" :placeholder="placeholder" @focus="onFocusInput"/>
+    <div class="f-date-picker-pop" v-if="popVisible">
+      <div class="f-date-picker-nav">
+        <span>
+          <f-icon name="left"></f-icon>
+          <f-icon name="left"></f-icon>
+        </span>
+        <span @click="onClickYear">
+          2019 年
+        </span>
+        <span @click="onClickMonth">
+          05 月
+        </span>
+        <span>
+          <f-icon name="left"></f-icon>
+          <f-icon name="left"></f-icon>
+        </span>
+      </div>
+      <div class="f-date-picker-panels">
+        <div class="f-date-picker-content" v-if="mode === 'years'">年</div>
+        <div class="f-date-picker-content" v-else-if="mode === 'months'">
+          月
+        </div>
+        <div class="f-date-picker-content" v-else>日</div>
+      </div>
+      <div class="f-date-picker-action"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import Input from "../input/Input";
+import Icon from "../icon/Icon";
 export default {
   name: "FlyDatePicker",
   components: {
-    "f-input": Input
+    "f-input": Input,
+    "f-icon": Icon
   },
   props: {
     placeholder: {
       type: String,
       default: "请选择日期"
     }
+  },
+  data() {
+    return {
+      popVisible: false,
+      mode: "days" //months,years
+    };
+  },
+  methods: {
+    onClickYear() {
+      this.mode = "years";
+    },
+    onClickMonth() {
+      this.mode = "months";
+    },
+    onFocusInput(){
+      this.popVisible = true
+    },
   }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.f-date-picker {
+  &-pop {
+    border: 1px solid red;
+    width: 300px;
+  }
+}
+</style>
