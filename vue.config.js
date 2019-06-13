8439071b5a8b8e34cb6015aca9598c8d375f0563
v_lzhicliu <v_lzhicliu@tencent.com>
2019-06-13 11:04:47 +0800

    设置_variable的alias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
 module.exports = {
     css: {
         loaderOptions: {
@@ -7,5 +10,9 @@ module.exports = {
                 includePaths: [path.join(__dirname, 'style')]
             }
         }
+    },
+    chainWebpack: (config)=>{
+        config.resolve.alias
+            .set('_variable', resolve('style/_variable'))
     }
 }
