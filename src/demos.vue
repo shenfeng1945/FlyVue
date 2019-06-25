1d02060de303d8fe93da6615e624fb0f9c210248
shenfeng1945 <823902826@qq.com>
2019-06-26 01:08:30 +0800

    新增switch控件

--- src/demos.vue
+++ src/demos.vue
@@ -1,35 +1,14 @@
 <template xmlns="http://www.w3.org/1999/XSL/Transform">
   <div id="app">
-    <f-nav :selected.sync="selected" direction="vertical">
-      <f-nav-item name="sports">体育</f-nav-item>
-      <f-sub-nav name="news">
-        <template slot="title">新闻</template>
-        <f-nav-item name="new1">网易新闻</f-nav-item>
-        <f-nav-item name="new2">腾讯新闻</f-nav-item>
-        <f-sub-nav name="toutiao">
-          <template slot="title">今日头条</template>
-          <f-nav-item name="toutiao1">xxxx</f-nav-item>
-          <f-nav-item name="toutiao2">yyyy</f-nav-item>
-          <f-nav-item name="toutiao3">zzzz</f-nav-item>
-        </f-sub-nav>
-      </f-sub-nav>
-      <f-nav-item name="finance" disabled>财经</f-nav-item>
-    </f-nav>
+      {{value}}
+    <f-switch v-model="value" @change="onChange"></f-switch>
   </div>
 </template>
 <script>
 import Input from "./input/Input";
 import Button from "./button/button";
 import Icon from "./icon/Icon";
-import Nav from './nav/nav';
-import NavItem from './nav/nav-item';
-import SubNav from './nav/sub-nav';
-import Tabs from './tabs/tabs';
-import TabsItem from './tabs/tabs-item';
-import TabsNav from './tabs/tabs-nav';
-import TabsPanel from './tabs/tabs-panel';
-import TabsContent from './tabs/tabs-content';
-
+import Switch from "./formControls/switch";
 
 export default {
   name: "Demos",
@@ -37,19 +16,19 @@ export default {
     "f-input": Input,
     "f-icon": Icon,
     "f-button": Button,
-    "f-nav": Nav,
-    "f-nav-item": NavItem,
-    "f-sub-nav": SubNav,
+    "f-switch": Switch
   },
   data() {
     return {
-      selected: []
+      value: true
     };
   },
   created() {
   },
   methods: {
-
+      onChange(val){
+          this.value = val;
+      }
   }
 };
 </script>
