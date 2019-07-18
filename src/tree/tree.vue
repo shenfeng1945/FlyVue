3063bf6271b11ec76cf4a5d7243425942260adf8
v_lzhicliu <v_lzhicliu@tencent.com>
2019-07-18 18:16:35 +0800

    创建tree组件

---
+++ src/tree/tree.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="f-tree">
+      <div v-for="(item,i) of data" :key="i">
+          <f-icon v-if="item.children && item.children.length" name="chevron-right"></f-icon>
+          {{item.label}}
+          <template v-if="item.children && item.children.length">
+            <FlyTree :data="item.children"></FlyTree>
+          </template>
+      </div>
+    </div>
+</template>
+
+<script>
+// 滑动输入条
+import Icon from "../icon/Icon";
+export default {
+    name: 'FlyTree',
+    props: {
+        data: {
+            type: Array,
+            default: () => []
+        }
+    },
+    components: {
+        'f-icon': Icon
+    }
+}
+</script>
+
