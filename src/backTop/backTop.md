4b555c0f44621bc7e52c9858c8294a93f5699560
v_lzhicliu <v_lzhicliu@tencent.com>
2019-07-18 09:44:18 +0800

    添加backTop功能

---
+++ src/backTop/backTop.md
@@ -0,0 +1,21 @@
+使用示例:
+
+```vue
+<f-back-top speed="quick">
+    <div
+      style="
+          height: 100%;
+          width: 100%;
+          background-color: #1088e9;
+          box-shadow: 0 0 6px rgba(0,0,0, .12);
+          line-height: 40px;
+          border-radius: 3px;
+          color: #fff;
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        "
+    >up</div>
+</f-back-top>
+```
--- src/backTop/backTop.vue
+++ src/backTop/backTop.vue
@@ -1,83 +1,99 @@
 <template>
-    <div class="f-back-top" :style="getStyle" v-show="targetVisible" @click="onBackTop">
+  <div class="f-back-top" :style="getStyle" v-show="targetVisible" @click="onBackTop">
+    <template v-if="$slots.default">
       <slot></slot>
-    </div>
+    </template>
+    <template v-else>
+      <f-button leftIcon="back-top" intent="primary" circle></f-button>
+    </template>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'FlyBackTop',
-    props: {
-        target: {
-            type: String,
-            default: '.list'
-        },
-        visibilityHeight: {
-            type: Number,
-            default: 200
-        },
-        right: {
-            type: Number,
-            default: 40,
-        },
-        bottom: {
-            type: Number,
-            default: 40
-        }
-    },
-    created(){
-       this.bindEvent();
-    },
-    mounted(){
-       this.onScrollEvent();
-    },
-    data(){
-        return {
-           targetVisible: false
-        }
-    },
-    computed: {
-        getStyle(){
-            return {
-                right: this.right + 'px',
-                bottom: this.bottom + 'px'
-            }
-        },
-    },
-    methods: {
-       bindEvent(){
-         document.addEventListener('scroll', this.onScrollEvent)
-       },
-       onScrollEvent(){
-          const targetEle = document.querySelector(this.target)
-          const {top} = targetEle.getBoundingClientRect()
-          if(Math.abs(top) > this.visibilityHeight){
-              this.targetVisible = true
-          }else{
-              this.targetVisible = false
-          }
-       },
-       onBackTop(){
-          const targetEle = document.querySelector(this.target)
-          let {top} = targetEle.getBoundingClientRect()
-          top = Math.abs(top);
-        //   if(top > 0){
-            //   window.requestAnimationFrame(this.onBackTop)
-            //   window.scrollTo(0, top - top/8)
-        //   }
-        targetEle.scrollTop = 0
-        // targetEle.scrollTo(0, 0)
-       }
-    }
+import Button from "../button/button";
+
+const speedObj = {
+    'slow': 10,
+    'middle': 8,
+    'quick': 6
 }
+export default {
+  name: "FlyBackTop",
+  props: {
+    target: {
+      type: String,
+      default: "body"
+    },
+    visibilityHeight: {
+      type: Number,
+      default: 400
+    },
+    right: {
+      type: Number,
+      default: 40
+    },
+    bottom: {
+      type: Number,
+      default: 40
+    },
+    speed: {
+        type: String,
+        default: 'middle',
+        validator(val){return ['slow', 'middle', 'quick'].indexOf(val) > -1}
+    }
+  },
+  components: { "f-button": Button },
+  created() {
+    this.bindEvent();
+  },
+  mounted() {
+    this.onScrollEvent();
+  },
+  data() {
+    return {
+      targetVisible: false
+    };
+  },
+  computed: {
+    getStyle() {
+      return {
+        right: this.right + "px",
+        bottom: this.bottom + "px"
+      };
+    }
+  },
+  methods: {
+    bindEvent() {
+      document.addEventListener("scroll", this.onScrollEvent);
+    },
+    onScrollEvent() {
+      const targetEle = document.querySelector(this.target);
+      const { top } = targetEle.getBoundingClientRect();
+      if (Math.abs(top) > this.visibilityHeight) {
+        this.targetVisible = true;
+      } else {
+        this.targetVisible = false;
+      }
+    },
+    onBackTop() {
+      const targetEle = document.querySelector(this.target);
+      let { top } = targetEle.getBoundingClientRect();
+      top = Math.abs(top);
+      if (top > 0) {
+        window.requestAnimationFrame(this.onBackTop);
+        window.scrollTo(0, top - top / speedObj[this.speed]);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .f-back-top{
-      position: fixed;
-      z-index: 9;
-      width: 32px;
-      height: 32px;
-  }
+.f-back-top {
+  position: fixed;
+  z-index: 9;
+  width: 32px;
+  height: 32px;
+}
 </style>
 
