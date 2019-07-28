3202d0a2f97e5d25d14899fb1aa9e9488d267d75
shenfeng1945 <823902826@qq.com>
2019-07-28 10:12:54 +0800

    删除map后缀

--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,6 @@ module.exports = {
     chainWebpack: (config)=>{
         config.resolve.alias
             .set('_variable', resolve('style/_variable'))
-    }
+    },
+    productionSourceMap: false
 }
