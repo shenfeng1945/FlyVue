f9ca479d06b97a3abef89b1d163ec85bc6bf1908
shenfeng1945 <823902826@qq.com>
2019-07-28 20:56:29 +0800

    sticky 支持底部定位和父组件触发位置重新计算

--- src/components/sticky/sticky.vue
+++ src/components/sticky/sticky.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="f-sticky-wrapper" ref="wrapper" :style="{height}">
-    <div class="f-sticky" :class="classes" :style="{left,width,top}">
+    <div class="f-sticky" :class="classes" :style="{left,width,top,bottom}">
       <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
+import {getViewPortHeight} from '@/utils/browser.js';
 export default {
   name: "FlySticky",
   props: {
     distance: {
       type: Number,
       default: 0
-    }
+    },
+    position: {
+      type: String,
+      default: "top",
+      validator(val) {
+        return ["top", "bottom"].indexOf(val) > -1;
+      }
+    },
+    eventBus: Object
   },
   data() {
     return {
@@ -21,23 +30,36 @@ export default {
       width: undefined,
       left: undefined,
       height: undefined,
-      top: undefined
+      top: undefined,
+      bottom: undefined,
+      viewportHeight: undefined,
     };
   },
+  created(){
+    this.eventBus && this.eventBus.$on('forceUpdate', this.forceUpdate);
+  },
   mounted() {
+    this.setViewPortHeight();
     this.windowScrollHandler = this._windowScrollHandler.bind(this);
     window.addEventListener("scroll", this.windowScrollHandler);
+    window.addEventListener('resize', this.setViewPortHeight)
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.windowScrollHandler);
+    window.removeEventListener('resize', this.setViewPortHeight)
   },
   methods: {
+    setViewPortHeight(){
+      this.viewportHeight = getViewPortHeight();
+    },
+    forceUpdate(){
+      this._windowScrollHandler();
+    },
     topAndHeight() {
       const { top, height } = this.$refs.wrapper.getBoundingClientRect();
       return { top: top + window.scrollY, height };
     },
-    _windowScrollHandler() {
-      const { top } = this.topAndHeight();
+    computedTopDistance(top) {
       if (window.scrollY > top - this.distance) {
         const {
           height,
@@ -47,7 +69,7 @@ export default {
         this.height = height + "px";
         this.left = left + "px";
         this.width = width + "px";
-        this.top = this.distance + 'px';
+        this.top = this.distance + "px";
         this.sticky = true;
       } else {
         this.height = undefined;
@@ -55,6 +77,30 @@ export default {
         this.top = undefined;
         this.width = undefined;
         this.sticky = false;
+      }
+    },
+    computedBottomDistance(top){
+      const {height, left, width} = this.$refs.wrapper.getBoundingClientRect();
+      if(window.scrollY < top - this.viewportHeight + height + this.distance){
+        this.height = height + "px";
+        this.left = left + "px";
+        this.width = width + "px";
+        this.bottom = this.distance + "px";
+        this.sticky = true;
+      }else{
+        this.height = undefined;
+        this.left = undefined;
+        this.bottom = undefined;
+        this.width = undefined;
+        this.sticky = false;
+      }
+    },
+    _windowScrollHandler() {
+      const { top } = this.topAndHeight();
+      if (this.position === "top") {
+        this.computedTopDistance(top);
+      }else {
+        this.computedBottomDistance(top);
       }
     }
   },
--- src/demos.vue
+++ src/demos.vue
@@ -1,11 +1,5 @@
 <template xmlns="http://www.w3.org/1999/XSL/Transform">
   <div id="app">
-      <f-water-fall :imageLists="data" @scrollReachBottom="updateData">
-          <template slot-scope="item">
-              <img :src="item.item.src" alt="">
-              <div class="description">{{item.item.name}}</div>
-          </template>
-      </f-water-fall>
   </div>
 </template>
 
@@ -13,41 +7,21 @@
 import Input from "@/components/input/Input";
 import Button from "@/components/button/button";
 import Icon from "@/components/icon/Icon";
-import WaterFall from "@/components/waterFall/waterFall";
+
 export default {
   name: "Demos",
   components: {
     "f-input": Input,
     "f-icon": Icon,
     "f-button": Button,
-    "f-water-fall": WaterFall
   },
   data() {
     return {
-      data: []
     };
   },
   created() {
-      this.data = Array.from({length: 10}, () => {
-      const path = Math.ceil(Math.random() * 18);
-      return {
-          src: require(`@/static/images/${path}.jpeg`),
-        name: `图片${path}`
-      }
-    });
   },
   methods: {
-    updateData(){
-       setTimeout(() => {
-           this.data = this.data.concat(Array.from({length: 10}, () => {
-               const path = Math.ceil(Math.random() * 18);
-               return {
-                   src: require(`@/static/images/${path}.jpeg`),
-                   name: `图片${path}`
-               }
-           }))
-       }, 500)
-    }
   }
 };
 </script>
