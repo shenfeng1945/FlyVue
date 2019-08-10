f6e0b796ff0f2380a51d4ac3efe97a4a98226adb
shenfeng1945 <823902826@qq.com>
2019-08-10 10:31:40 +0800

    添加 datepicker 文档

--- docs/.vuepress/components/datePicker-demos.vue
+++ docs/.vuepress/components/datePicker-demos.vue
@@ -1,15 +1,84 @@
 <template>
-  <div>hello</div>
+  <div class="docs-data-picker">
+    <f-init-docs></f-init-docs>
+    
+    <h4>选择日</h4>
+    <p>以「日」为基本单位，基础的日期选择控件</p>
+    <f-card-group :source="getDatePickerSource">
+       <f-date-picker :value="value" @input="value = $event" :showActions="showBarActionValue" :reserveMonthAndYear="reverseMonthValue" :highlightCurrentDay="highlightCurrentDay" :readonly="readonlyValue" :clearable="clearableValue"/>
+       <template slot="props">
+        <h5 class="f-heading">Props</h5>
+        <f-switch v-model="readonlyValue" @change="readonlyValue = $event">Readonly</f-switch>
+        <f-switch v-model="showBarActionValue" @change="showBarActionValue = $event">Show actions bar</f-switch>
+        <f-switch v-model="highlightCurrentDay" @change="highlightCurrentDay = $event">Highlight current day</f-switch>
+        <f-switch v-model="reverseMonthValue" @change="reverseMonthValue = $event">Reverse month and year</f-switch>
+        <f-switch v-model="clearableValue" @change="clearableValue = $event">Clearable</f-switch>
+       </template>
+    </f-card-group>
+  </div>
 </template>
 
 <script>
+import InitDocs from "@/docs-components/initDocs";
+import ExampleGroupCard from "@/docs-components/exampleGroupCard";
+import DatePicker from '@/components/datepicker/datepicker';
+import Switch from "@/components/formControls/switch";
 export default {
-  name: 'DatePickerDemos' 
+  name: 'DatePickerDemos' ,
+  components: {
+    "f-init-docs": InitDocs,
+    "f-card-group": ExampleGroupCard,
+    "f-date-picker": DatePicker,
+    "f-switch": Switch,
+  },
+  computed: {
+    getDatePickerSource(){
+      const propsString = `${this.readonlyValue ? 'readonly' : ''} ${this.showBarActionValue ? 'showActions' : ''} ${this.highlightCurrentDay ? 'highlightCurrentDay' : ''} ${this.reverseMonthValue ? 'reserveMonthAndYear' : ''} ${this.clearableValue ? 'clearable' : ''}`
+      return {
+        desc: '',
+        code: `
+<template>
+  <f-date-picker :value="value" @input="value = $event" ${propsString.trim()}/>
+</template>
+
+<script>
+ export default {
+   data(){
+     return {
+       value: null
+     }
+   }
+ }
+<\/script>
+        
+        `
+      }
+    }
+  },
+  data(){
+    return {
+      value: null,
+      showBarActionValue: false,
+      highlightCurrentDay: false,
+      reverseMonthValue: false,
+      readonlyValue: false,
+      clearableValue: true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.f-heading {
+  color: #182026;
+  font-weight: 600;
+  margin: 0 0 10px;
+  padding: 0;
+  font-size: 16px;
+}
+.f-switch {
+    margin-bottom: 10px;
+}
 </style>
 
 
--- docs/views/components/datePicker.md
+++ docs/views/components/datePicker.md
@@ -11,3 +11,22 @@ sidebarDepth: 2
   <datePicker-demos />
 </ClientOnly>
 
+### Attributes
+
+| 参数 | 说明 | 类型 | 可选择 | 默认值 |
+| -- | -- | -- | -- | -- |
+| value | 绑定值 | date/null | - | null |
+| readonly | 完全只读 | boolean | - | false |
+| clearable | 输入框是否显示清除按钮 | boolean | - | true |
+| placeholder | 输入框占位文本 | string | - | 请选择日期 |
+| showActions | 日期框是否带快捷项 | boolean | - | false |
+| reserveMonthAndYear | 日期框中月份与年是否对调 | boolean | - | false |
+| highlightCurrentDay | 日期框中是否高亮今日日期 | boolean | - | false |
+
+### Events
+
+
+| 参数 | 说明 | 回调参数 | 
+| -- | -- | -- | 
+| input | 用户确认选定的值时触发 | (value: Date \| Object) |
+
