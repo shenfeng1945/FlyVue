73f2efa0db83c063f6da936c66ddd42222072128
shenfeng1945 <823902826@qq.com>
2021-02-09 18:08:10 +0800

    新增 switch 单元测试

---
+++ tests/unit/switch.spec.js
@@ -0,0 +1,14 @@
+import {expect} from 'chai'
+import Vue from 'vue'
+import Switch from '@/components/formControls/switch'
+import { mount } from '@vue/test-utils'
+
+
+describe('Switch', () => {
+    it('存在', () => {
+        expect(Switch).to.exist;
+    })
+    it('接受 value 属性', () => {
+       
+    })
+})
